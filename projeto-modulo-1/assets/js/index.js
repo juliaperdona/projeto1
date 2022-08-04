080ba11177eaa80e387baf4656e7e79ba8a5bd8c
juliaperdona <juliaperdona@gmail.com>
2022-08-03 22:51:59 -0300

    commit

---
+++ projeto-modulo-1/assets/js/index.js
@@ -0,0 +1,10 @@
+const titulo = document.getElementById(titulo)
+const linguagem = document.getElementById(linguagem)
+const categoria = document.getElementById(categoria1)
+const descricao = document.getElementById(descricao)
+const salvar = document.getElementById(salvar)
+const limpar = document.getElementById(limpar)
+
+function salvarDica(titulo, linguagem, categoria, descricao){
+    
+}
