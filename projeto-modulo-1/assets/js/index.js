e6c00fe678906c9b2df73b17827ebb0f0a4a75a1
juliaperdona <juliaperdona@gmail.com>
2022-08-05 22:26:24 -0300

    commit

--- projeto-modulo-1/assets/js/index.js
+++ projeto-modulo-1/assets/js/index.js
@@ -1,7 +1,9 @@
 const formulario = document.getElementById("formulario");
 formulario.onsubmit = salvarDica;
 
-function salvarDica(event){
+var dados = JSON.parse(localStorage.getItem("Dados")) || [];
+
+function salvarDica(event) {
     event.preventDefault();
 
     const titulo = document.getElementById('titulo').value
@@ -10,35 +12,40 @@ function salvarDica(event){
     const descricao = document.getElementById('descricao').value
     const video = document.getElementById('video').value
 
- var dados = JSON.parse(localStorage.getItem("Dados"));
 
- if(dados == null){
-    localStorage.setItem("Dados", "[]")
-    dados = []
- }
-const card = {
-    id : new Date().getTime(),
-    titulo: titulo,
-    linguagem : linguagem,
-    categoria : categoria,
-    descricao : descricao,
-    video : video,
+    if (dados == null) {
+        localStorage.setItem("Dados", "[]")
+        dados = []
+    }
+    const card = {
+        id: new Date().getTime(),
+        titulo: titulo,
+        linguagem: linguagem,
+        categoria: categoria,
+        descricao: descricao,
+        video: video,
+
+    }
+    dados.push(card);
+    localStorage.setItem("Dados", JSON.stringify(dados))
+
+    total()
+    totalFrontEnd()
+    totalBackEnd()
+    totalFullStack()
+    totalSoftSkills()
+    criarCard()
+    alert("Dica cadastrada!")
 
 }
- dados.push(card);
- localStorage.setItem("Dados", JSON.stringify(dados))
 
- criarCard()
-
-}
-
-function criarCard(){
+function criarCard() {
     const dados = JSON.parse(localStorage.getItem("Dados"));
     const listaCards = document.getElementById('lista-cards')
     let card = ''
     dados.forEach(element => {
-        card += 
-        
+        card +=
+
             `<li id="card-dica">
                 <h3>${element.titulo}</h3>
                 <h5>Linguagem/Skill:${element.linguagem}</h5>
@@ -47,17 +54,99 @@ function criarCard(){
                         <div class="botoes">
                             <a href="${element.video}" target="_blank"><button>Vídeo</button></a>
                             <button onclick="editar">Editar</button>
-                            <button onclick="excluir()" id="excluir">Excluir</button>
+                            <button onclick="excluir(${element.id})" id="excluir">Excluir</button>
                         </div>
             </li>`
-            listaCards.innerHTML = card
-        
+        listaCards.innerHTML = card
+
     });
 }
 
 
-function excluir(){
-    var dados = JSON.parse(localStorage.getItem("Dados"))
-    var id = localStorage.getItem('id')
-   console.log(id)
+function excluir(elementId) {
+    if (window.confirm("Você deseja excluir?")) {
+
+        console.log(elementId)
+        dados = dados.filter(({ id }) => {
+            return id != elementId
+        });
+
+        localStorage.setItem("Dados", JSON.stringify(dados))
+
+        totalFrontEnd()
+        totalBackEnd()
+        totalFullStack()
+        totalSoftSkills()
+        total()
+        criarCard()
+        alert("Dica excluída")
+    }
 }
+
+criarCard()
+
+function total() {
+    let contadorCards = dados.length;
+    let contadorTotal = document.getElementById("total");
+    contadorTotal.innerText = contadorCards
+}
+
+total()
+
+function totalFrontEnd() {
+    function frontEnd(element) {
+        return element.categoria == "frontEnd"
+    }
+
+    let contadorFrontEnd = dados.filter(frontEnd)
+    let contadorTotalFront = contadorFrontEnd.length
+    let contadorTotal = document.getElementById("totalFrontEnd");
+    contadorTotal.innerText = contadorTotalFront
+}
+totalFrontEnd()
+
+function totalBackEnd() {
+    function backEnd(element) {
+        return element.categoria == "BackEnd"
+    }
+
+    let contadorBackEnd = dados.filter(backEnd)
+    let contadorTotalBack = contadorBackEnd.length
+    let contadorTotal = document.getElementById("totalBackEnd");
+    contadorTotal.innerText = contadorTotalBack
+}
+totalBackEnd()
+
+function totalFullStack() {
+    function fullStack(element) {
+        return element.categoria == "fullStack"
+    }
+    let contadorFullStack = dados.filter(fullStack)
+    let contadorTotalFull = contadorFullStack.length
+    let contadorTotal = document.getElementById("totalFullStack");
+    contadorTotal.innerText = contadorTotalFull
+}
+totalFullStack()
+
+function totalSoftSkills() {
+
+    function softSkills(element) {
+        return element.categoria == "softSkills"
+    }
+
+    let contadorSoftSkills = dados.filter(softSkills)
+    let contadorTotalSoft = contadorSoftSkills.length
+    let contadorTotal = document.getElementById("totalSoftSkills");
+    contadorTotal.innerText = contadorTotalSoft
+}
+totalSoftSkills()
+
+function buscar() {
+    function busca() {
+        return document.getElementById("pesquisar")
+    }
+    let pesquisa = dados.filter(busca)
+    
+
+}
+buscar()
